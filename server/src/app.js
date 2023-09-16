80dfc8c792c605734c50d29b180717be8a018089
Imagine96 <rubged9606@gmail.com>
2023-09-16 11:58:35 +0200

    balance display in client

--- server/src/app.js
+++ server/src/app.js
@@ -1,7 +1,10 @@
 import express from 'express'
 import { getBalance } from './controllers/transaction.controller.js'
+import cors from 'cors'
 
 const app = express()
+app.use(cors()) 
+app.use(express.json())
 
 app.get("/balance", getBalance)
 app.post("/deposit", (_, res) => res.status(200).json("deposit"))
